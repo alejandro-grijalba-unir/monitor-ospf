5ef09a03a5507d0c4e3d7b1786c00b795c63a8f4
alejandro-grijalba-unir <85701003+alejandro-grijalba-unir@users.noreply.github.com>
2021-06-10 20:02:27 +0200

    Update urls.py

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', include('monitor.urls')),
 #    path('myapp', include('myapp.urls')),
 ]
