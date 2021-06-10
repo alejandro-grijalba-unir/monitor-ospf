fc805c04c1d46df02d42d4527583c8719be22232
alejandro-grijalba-unir <85701003+alejandro-grijalba-unir@users.noreply.github.com>
2021-06-10 19:49:09 +0200

    Update apps.py

--- monitor/apps.py
+++ monitor/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyappConfig(AppConfig):
+class MonitorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myapp'
+    name = 'monitor'
