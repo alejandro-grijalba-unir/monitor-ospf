ef60e4d9d13cea68a1d5882d31e6718303541662
alex <alex@aceleron>
2022-03-11 18:12:10 +0100

    Permitir acceso remoto a la app

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7i)z&nb%e@)tc$5+&ah!+3rij)xi0^31=by0xwi10o^pu^7trg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
